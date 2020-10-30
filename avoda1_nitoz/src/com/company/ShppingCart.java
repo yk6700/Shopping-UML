3e53cbc31c8af0a6dc5f00d6be4406dc874780c6
GuyArieli17 <guyarieli17@gmail.com>
2020-10-30 12:48:17 +0200

    WEB_USER,ShoppingCart
    שינוי ב WEB USE
    ShoppingCart

--- avoda1_nitoz/src/com/company/ShppingCart.java
+++ avoda1_nitoz/src/com/company/ShppingCart.java
@@ -4,4 +4,12 @@ import java.util.Date;
 
 public class ShppingCart {
     private Date created;
+    private Account account;
+    private ArrayList<LineItem> lineItems;
+
+
+    public Date getCreated() {
+        return created;
+    }
+
 }
--- avoda1_nitoz/src/com/company/WebUser.java
+++ avoda1_nitoz/src/com/company/WebUser.java
@@ -4,4 +4,19 @@ public class WebUser {
     private String login_id;
     private String password;
     private UserState state;
+
+    private Customer customer;
+
+    public String getLogin_id() {
+        return login_id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserState getState() {
+        return state;
+    }
+
 }
