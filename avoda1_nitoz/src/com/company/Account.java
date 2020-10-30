5b6ff3cd49a43466d4e9fedde622840e7e5d88e9
yk6700 <yk6700@users.noreply.github.com>
2020-10-30 12:09:53 +0200

    Create Account.java

---
+++ avoda1_nitoz/src/com/company/Account.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Account {
+    private String id;
+    private String billing_address;
+    private boolean is_closed;
+    private Date open;
+    private Date closed;
+    private int balance;
+
+}
