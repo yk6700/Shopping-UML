6f2c4a7a81db18cabab8613b84439a9101bb13ac
yk6700 <yk6700@users.noreply.github.com>
2020-10-30 12:10:14 +0200

    Create Order.java

---
+++ avoda1_nitoz/src/com/company/Order.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Order {
+    private String number;
+    private Date Ordered;
+    private Date shipprd;
+    private Address ship_to;
+    private OrderStatus status;
+    private float total;
+}
