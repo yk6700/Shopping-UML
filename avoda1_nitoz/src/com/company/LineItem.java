8a497e1231ca907bd50c871ce5daca59a11bf6bb
ronny5423 <ronny54@gmail.com>
2020-10-31 10:38:34 +0200

    Shopping cart update

--- avoda1_nitoz/src/com/company/LineItem.java
+++ avoda1_nitoz/src/com/company/LineItem.java
@@ -6,47 +6,44 @@ public class LineItem {
     private ShoppingCart shoppingCart;
     private Order order;
     private Product product;
-
+    
     public LineItem(int quantity, int price, ShoppingCart shoppingCart, Order order, Product product) {
         this.quantity = quantity;
         this.price = price;
         this.shoppingCart = shoppingCart;
         boolean s = shoppingCart.addLineItem(this);
-        if (!s)
+        if(!s)
             throw new RuntimeException("line item already exist");
-
+        
         this.order = order;
         boolean o = order.addLineItem(this);
-        if (!o)
+        if(!o)
             throw new RuntimeException("line item already exist");
-
+        
         this.product = product;
         boolean p = product.addLineItem(this);
-        if (!p)
+        if(!p)
             throw new RuntimeException("line item already exist");
     }
-
+    
     public int getQuantity() {
         return quantity;
     }
-
+    
     public int getPrice() {
         return price;
     }
-
+    
     public ShoppingCart getShoppingCart() {
         return shoppingCart;
     }
-
+    
     public Order getOrder() {
         return order;
     }
-
+    
     public Product getProduct() {
         return product;
     }
-
-    public ShoppingCart getShoppingCart() {
-        return shoppingCart;
-    }
 }
+
--- avoda1_nitoz/src/com/company/ShoppingCart.java
+++ avoda1_nitoz/src/com/company/ShoppingCart.java
@@ -1,9 +1,40 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class ShoppingCart {
     private Date created;
-
-
+    private Account account;
+    private WebUser webUser;
+    private ArrayList<LineItem> lineItems;
+    
+    public ShoppingCart(Date created, Account account, WebUser webUser) {
+        this.created = created;
+        lineItems=new ArrayList<>();
+        if(account.getShoppingCart()==null && webUser.getShoppingCart()==null){
+            this.webUser=webUser;
+            this.account=account;
+        }
+        else{
+            throw new RuntimeException("Shopping cart can be connected only to one account and one web user");
+        }
+    }
+    
+    public void addLineItem(LineItem l){
+        if(l.getShoppingCart()==null){
+            lineItems.add(l);
+        }
+        else{
+            throw new RuntimeException("line item can be connected only to one shopping cart");
+        }
+    }
+    
+    public Account getAccount() {
+        return account;
+    }
+    
+    public WebUser getWebUser() {
+        return webUser;
+    }
 }
